bf89d0a7b28ec4337fa0bb9531658a21befe7399
Michael Johan Bosco <mwatopa@gmail.com>
2026-09-07 06:26:29 +0700

    fix(pwa): invalidate stale KerjaHarian service-worker cache

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'kerjaharian-v2';
+const CACHE_NAME = 'kerjaharian-v3';
 const STATIC_ASSETS = ['/','/index.html','/manifest.json','/favicon.svg'];
 
 self.addEventListener('install', (event) => {
