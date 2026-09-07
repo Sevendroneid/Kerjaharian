5470c7499ce8f4a48bd9c6a4f7e309877c271f2c
Michael Johan Bosco <mwatopa@gmail.com>
2026-09-07 16:00:19 +0700

    feat: add optional Google Analytics 4 integration

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,22 @@ import App from './App.tsx';
 import { AuthProvider } from './lib/auth';
 import './index.css';
 
+const measurementId = import.meta.env.VITE_GA_MEASUREMENT_ID as string | undefined;
+if (measurementId && /^G-[A-Z0-9]+$/i.test(measurementId)) {
+  const script = document.createElement('script');
+  script.async = true;
+  script.src = `https://www.googletagmanager.com/gtag/js?id=${measurementId}`;
+  document.head.appendChild(script);
+  window.dataLayer = window.dataLayer || [];
+  window.gtag = function gtag(...args: unknown[]) { window.dataLayer.push(args); };
+  window.gtag('js', new Date());
+  window.gtag('config', measurementId, { anonymize_ip: true });
+}
+
+declare global {
+  interface Window { dataLayer: unknown[]; gtag: (...args: unknown[]) => void; }
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <AuthProvider>
