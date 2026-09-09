fd30731ae90a7535b81227e93f8786d8511f0920
Michael Johan Bosco <mwatopa@gmail.com>
2026-09-09 22:40:07 +0700

    ci: allow intentional empty catch in admin voice recorder

--- eslint.config.js
+++ eslint.config.js
@@ -22,6 +22,7 @@ export default tseslint.config(
       '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
       '@typescript-eslint/no-explicit-any': 'warn',
       'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+      'no-empty': ['error', { allowEmptyCatch: true }],
     },
   }
 );
